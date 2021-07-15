85f0604d3f99f32d26a70016661496a93a67f2da
Suhail shariff <suhailshariff907@gmail.com>
2021-07-15 22:57:11 +0530

    Added PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './/app-routing.module';
 import { AppComponent } from './app.component';
 import { SiteLayoutComponent } from './application-layout/site-layout/site-layout.component';import { SiteLayoutService } from '@layout/site-layout.service';
 import { PortfolioComponent } from './site-layout/portfolio/portfolio.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,13 @@ import { PortfolioComponent } from './site-layout/portfolio/portfolio.component'
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CommonModule
+    CommonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   exports: [],
   providers: [SiteLayoutService],
